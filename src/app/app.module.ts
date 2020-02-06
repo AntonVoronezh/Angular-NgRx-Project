2672f6b65a715292b527e05cf06339cab169d9f7
AntonVoronezh <pstut@mail.ru>
2020-02-06 11:51:44 +0300

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsFormComponent } from './cars-form/cars-form.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsFormComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cars-form/cars-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-form',
+  templateUrl: './cars-form.component.html',
+  styleUrls: ['./cars-form.component.scss']
+})
+export class CarsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
