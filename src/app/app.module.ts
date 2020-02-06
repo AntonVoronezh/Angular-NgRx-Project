57a082b053678cc31cf246d20273caabf017a999
AntonVoronezh <pstut@mail.ru>
2020-02-06 14:20:32 +0300

    add store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from "@angular/forms";
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {StoreModule} from "@ngrx/store";
 
-import { AppComponent } from './app.component';
 import { CarsFormComponent } from './cars-form/cars-form.component';
+import { AppComponent } from './app.component';
 import { CarComponent } from './car/car.component';
+import {carsReducer} from "./redux/cars.reducer";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { CarComponent } from './car/car.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    StoreModule.forRoot({carPage: carsReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
