3267d621098afee8bf83b08a01196e5d9e45484c
AntonVoronezh <pstut@mail.ru>
2020-02-06 14:43:49 +0300

    extract from store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,36 @@
-import {Component} from '@angular/core';
-import {Car} from "./car.model";
+import {Component, OnInit} from '@angular/core';
+import {Store} from "@ngrx/store";
+
+import {Car, Cars} from "./car.model";
+import {AppState} from "./redux/app.state";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public cars: Car[] = [ ]
+export class AppComponent implements OnInit{
+  // public cars: Car[] = [];
+  public carState: Observable<Cars>;
+
+  constructor(private store: Store<AppState>) {
+  }
+
+  ngOnInit(): void {
+    // this.store.select('carPage').subscribe(({cars}) => {
+    //   this.cars = cars;
+    // })
+    this.carState = this.store.select('carPage');
+  }
 
   onAdd(car: Car) {
-    this.cars.push(car);
+    // this.cars.push(car);
   }
 
   onDelete(car: Car) {
-    this.cars = this.cars.filter(c => c.id !== car.id);
+    // this.cars = this.cars.filter(c => c.id !== car.id);
   }
+
+
 }
